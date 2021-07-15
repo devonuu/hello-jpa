7a52dcd98b49dba7b05963a03b65dedec9d45b61
Gunwoo Park <prayzz12@gmail.com>
2021-07-15 19:16:50 +0900

    address 추가 및 persistence.xml 제거

--- src/main/java/jpa_basic/hellojpa/domain/Address.java
+++ src/main/java/jpa_basic/hellojpa/domain/Address.java
@@ -1,5 +1,6 @@
 package jpa_basic.hellojpa.domain;
 
+import java.util.Objects;
 import javax.persistence.Embeddable;
 import lombok.Getter;
 
@@ -16,5 +17,24 @@ public class Address {
         this.city = city;
         this.street = street;
         this.zipcode = zipcode;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Address address = (Address) o;
+        return Objects.equals(getCity(), address.getCity()) && Objects
+            .equals(getStreet(), address.getStreet()) && Objects
+            .equals(getZipcode(), address.getZipcode());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCity(), getStreet(), getZipcode());
     }
 }
